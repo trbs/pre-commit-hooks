0b859aa335afe2faa42a078d2f157f91019df422
trbs <trbs@trbs.net>
2017-11-06 23:20:53 +0100

    add mypy

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='pre-commit-hooks-trbs',
     description='pre-commit hooks from trbs',
     url='https://github.com/trbs/pre-commit-hooks-trbs',
-    version='1.2.1',
+    version='1.2.2',
 
     author='Trbs',
     author_email='trbs@trbs.net',
